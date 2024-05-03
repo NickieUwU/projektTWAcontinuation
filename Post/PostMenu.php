139b0cc88d5d48552d7c3d569f2c7deb189f8344
VlastimilVasut <vasutvlastimil@gmail.com>
2024-05-03 11:55:22 +0200

    Working on post menu

--- Post/PostMenu.php
+++ Post/PostMenu.php
@@ -1,24 +1,22 @@
 <?php
     $Post_ID = $postID;
 ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link rel="stylesheet" href="../Post/PostMenu.css?v=<?php echo time(); ?>">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <script src="../Post/PostMenuLoggedUser.js"></script>
 <div class="MenuDiv">
     <span class="classMenuDots bi bi-three-dots-vertical" id="MenuDots" onclick="toggleMenu()"></span>
-    <table id="MenuTable" style="display: none;">
+    <table id="MenuTable" class="" style="display: none;">
         <tbody>
             <tr>
-                <td class="report">
+                <td class="table-default">
                     <span class="bi bi-flag-fill"></span>
                 </td>
                 <td>
-                    <span>report</span>
+                    <span class="table-default">report</span>
                 </td>
             </tr>
             <tr>
-                <td>
+                <td class="table-danger">
                     <span>block</span>
                 </td>
             </tr>
