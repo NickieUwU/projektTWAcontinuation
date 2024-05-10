447d2bbff645848b9467deb5df6ca996f7adb011
VlastimilVasut <vasutvlastimil@gmail.com>
2024-05-10 15:31:45 +0200

    Improving the menu

--- Post/PostMenu.php
+++ Post/PostMenu.php
@@ -5,21 +5,13 @@
 <script src="../Post/PostMenuLoggedUser.js"></script>
 <div class="MenuDiv">
     <span class="classMenuDots bi bi-three-dots-vertical" id="MenuDots" onclick="toggleMenu()"></span>
-    <table id="MenuTable" class="" style="display: none;">
-        <tbody>
-            <tr>
-                <td class="table-default">
-                    <span class="bi bi-flag-fill"></span>
-                </td>
-                <td>
-                    <span class="table-default">report</span>
-                </td>
-            </tr>
-            <tr>
-                <td class="table-danger">
-                    <span>block</span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div id="MenuTable" class="table table-dark" style="display: none;">
+        <div class="report">
+            <i class="icons bi bi-flag-fill"></i>
+            <span>report</span>
+        </div>
+        <div class="block">
+
+        </div>
+    </div>
 </div>
