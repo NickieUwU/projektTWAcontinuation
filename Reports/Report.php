dde9544a8a110d526d654bce81f0e0c58ba63524
VlastimilVasut <vasutvlastimil@gmail.com>
2024-05-13 21:14:15 +0200

    Making reports page

--- Reports/Report.php
+++ Reports/Report.php
@@ -30,5 +30,33 @@
     <?php 
         require("../Nav/Nav.php");
     ?>
+    <div class="whereamI">
+        <label class="lblMyProfile">Report <?php echo $PosterUsername; ?>'s post</label>
+    </div>
+    <div class="Report">
+        <form action="Report.php?Post=<?php echo $PostID; ?>&username=<?php echo $username; ?>" method="post">
+            <select name="ReportOptions" class="ReportOptions">
+                <option value="Spamming">Spamming</option>
+                <option value="Fraud">Fraud</option>
+                <option value="Bullying">Bullying</option>
+                <option value="Copyright">Copyright</option>
+            </select><br><br><br><br><br>
+            <input type="text" placeholder="what happened?" name="explanation" max="50"><br>
+            <input class="btn btn-outline-secondary" type="submit" value="Report">
+        </form>
+    </div>
 </body>
 </html>
+
+<?php
+    if($_SERVER["REQUEST_METHOD"] === "POST")
+    {
+        $ReportOption = $_POST["ReportOptions"];
+        $explanation = $_POST["explanation"];
+        if($explanation != null)
+        {
+            $data = array("ReportOption" => $ReportOption, "ReportExplanation" => $explanation);
+            Db::insert("reports", $data);
+        }
+    }
+?>
